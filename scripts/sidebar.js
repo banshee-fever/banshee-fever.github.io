fd853d67a70ebc374e48fe68d59e8a11812a4123
banshee-fever <56455683+banshee-fever@users.noreply.github.com>
2022-10-01 19:27:51 +0100

    initial commit. testing pages only

---
+++ scripts/sidebar.js
@@ -0,0 +1,19 @@
+document.write(`
+<div class="column sidebar">
+    <div class="header">
+        <a href="./index.html" id="logo"><img id="pfp" src="./assets/img/sidebar/pfp_202207.png" title="top-img"></a>
+        <h1>Banshee Fever</h1>
+    </div>
+    <div id="navigation">
+        <a href="projects.html"><img class="nav-icon" style="vertical-align:middle" src="./assets/img/sidebar/folder.png"> Projects </a><br />
+        <a href="blog.html"><img class="nav-icon" style="vertical-align:middle" src="./assets/img/sidebar/notepad.png"> Blog </a><br />
+        <a href="links.html"><img class="nav-icon" style="vertical-align:middle" src="./assets/img/sidebar/links.png"> Links </a><br/>
+
+        <a href="https://github.com/banshee-fever" title="GitHub" target="_blank"><img class="nav-icon" style="vertical-align:middle" src="./assets/img/sidebar/github.png"> Github </a><br/>
+        <a href="https://github.com/banshee-fever/banshee-fever.github.io" target="_blank"><small>Website Source</small></a>
+    </div>
+</div>
+<div class="column sidebar-border">
+    <!--bruh-->
+</div>
+`);
